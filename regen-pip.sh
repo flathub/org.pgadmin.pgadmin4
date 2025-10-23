ead7fa31e0c268bd0a5d0f0660ea3b67b77c821b
Kolja Lampe <klampe@slashwhy.de>
2025-10-23 11:13:31 +0200

    Update pgadmin4-server and pgadmin modules
    pgadmin4-server: Update pgadmin-server.deb to 9.9
    pgadmin: Update pgadmin-desktop.deb to 9.9

--- regen-pip.sh
+++ regen-pip.sh
@@ -1,5 +1,5 @@
 # Increase this on each version update and rerun the script
-wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_8/requirements.txt
+wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_9/requirements.txt
 
 # https://github.com/flatpak/flatpak-builder-tools/issues/365
 cat requirements.txt | grep -v "<= '3.9'" | grep -v sys_platform==\"win32\" > requirements_filtered.txt
@@ -7,7 +7,9 @@ sed -i "1 i tomli" requirements_filtered.txt # psycopg-c requires tomli to build
 sed -i "1 i greenlet" requirements_filtered.txt # psycopg-c requires greenlet to build
 sed -i "1 i flit-core" requirements_filtered.txt # tomli requires flit_core.buildapi to build
 
-flatpak-pip-generator --yaml -r requirements_filtered.txt --ignore-pkg bcrypt==4.3.* cryptography==45.0.*
+uv run flatpak-pip-generator --yaml \
+        --requirements-file=requirements_filtered.txt \
+        --ignore-pkg bcrypt==5.0.* cryptography==46.0.*
 
 rm requirements.txt
 rm requirements_filtered.txt
