5e55e3724da867412dfe5cbbc38b0e2971b9bfbc
Kolja Lampe <razzeee@gmail.com>
2025-11-18 01:17:47 +0100

    Update requirements file URL for pgAdmin 4 to version 9.10

--- regen-pip.sh
+++ regen-pip.sh
@@ -1,11 +1,9 @@
 # Increase this on each version update and rerun the script
-wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_9/requirements.txt
+wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_10/requirements.txt
 
 # https://github.com/flatpak/flatpak-builder-tools/issues/365
 cat requirements.txt | grep -v "<= '3.9'" | grep -v sys_platform==\"win32\" > requirements_filtered.txt
-sed -i "1 i tomli" requirements_filtered.txt # psycopg-c requires tomli to build
-sed -i "1 i greenlet" requirements_filtered.txt # psycopg-c requires greenlet to build
-sed -i "1 i flit-core" requirements_filtered.txt # tomli requires flit_core.buildapi to build
+sed -i "1 i pybind11" requirements_filtered.txt # pillow requires pybind11 to build
 
 uv run flatpak-pip-generator --yaml \
         --requirements-file=requirements_filtered.txt \
