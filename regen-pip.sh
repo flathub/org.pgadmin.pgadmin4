2645d69a49d41a1e44699694a9e371e58cd44fc2
Razze <razzeee@gmail.com>
2026-09-21 11:30:29 +0200

    Regenerate Python dependencies for pgAdmin 9.18

--- regen-pip.sh
+++ regen-pip.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -eu
+
 # Increase this on each version update and rerun the script
 PGADMIN_VERSION=9_18
 
@@ -9,9 +12,10 @@ sed -i "1 i pybind11" requirements_filtered.txt # pillow requires pybind11 to bu
 
 # Download latest flatpak-pip-generator.py and run
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/pip/flatpak-pip-generator.py
-uv run --with pyyaml ./flatpak-pip-generator.py --yaml \
+uv run --python 3.13 --with pip --with pyyaml ./flatpak-pip-generator.py --yaml \
         --requirements-file=requirements_filtered.txt \
-        --ignore-pkg bcrypt==5.0.* cryptography==50.0.*
+        --ignore-installed setuptools \
+        --ignore-pkg bcrypt,cryptography
 
 rm requirements.txt
 rm requirements_filtered.txt
